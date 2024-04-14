357cb7a8e232220aa1f4a8c6e356b40906e84738
Кирилл Попов <diplomatgmg@gmail.com>
2024-04-14 20:40:29 +0300

    Реализован выбор валюты

--- src/components/CurrencySelect.tsx
+++ src/components/CurrencySelect.tsx
@@ -1,11 +1,20 @@
-import React, { type ReactElement } from 'react'
+import React, { type ChangeEvent, type ReactElement, useState } from 'react'
+import currencies, { type Currency, defaultCurrency } from '../constants/currencies'
 
 const CurrencySelect = (): ReactElement => {
+  const [selectedCurrency, setSelectedCurrency] = useState<Currency >(defaultCurrency)
+
+  const handleSelectCurrency = (e: ChangeEvent<HTMLSelectElement>): void => {
+    const newCurrency = e.target.value
+    setSelectedCurrency(currencies.find(item => item.label === newCurrency) ?? defaultCurrency)
+  }
+
   return (
    <div className="converter__select-wrapper">
-     <img src="https://flagcdn.com/48x36/us.png" alt="" className="converter__flag"/>
+     <img src={`https://flagcdn.com/48x36/${selectedCurrency.value.toLowerCase()}.png`} alt="" className="converter__flag"/>
      <label className="converter__select-label">
-       <select className="converter__select">
+       <select className="converter__select" value={selectedCurrency?.label ?? 'USD'} onChange={handleSelectCurrency}>
+         {currencies.map(({ label }) => <option key={label} value={label}>{label}</option>)}
          <option value="RUB">RUB</option>
          <option value="USD">USD</option>
          <option value="RUB">EUR</option>
