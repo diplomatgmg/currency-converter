cd62f732ddce81e48d9898dde224b81a5506e93d
Кирилл Попов <diplomatgmg@gmail.com>
2024-04-14 21:41:24 +0300

    Реализовано управление состоянием

---
+++ src/redux/currencySlice.ts
@@ -0,0 +1,39 @@
+import { type Currency, defaultCurrency } from '../constants/currencies'
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit'
+
+interface CurrencyState {
+  amount: string
+  fromCurrency: Currency
+  toCurrency: Currency
+}
+
+const initialCurrencyState: CurrencyState = {
+  amount: '',
+  fromCurrency: defaultCurrency,
+  toCurrency: defaultCurrency
+}
+
+const currencySlice = createSlice({
+  name: 'currency',
+  initialState: initialCurrencyState,
+  reducers: {
+    setAmount (state, action: PayloadAction<string>) {
+      state.amount = action.payload
+    },
+    setFromCurrency (state, action: PayloadAction<Currency>) {
+      state.fromCurrency = action.payload
+    },
+    setToCurrency (state, action: PayloadAction<Currency>) {
+      state.toCurrency = action.payload
+    }
+  }
+})
+
+export const {
+  setAmount,
+  setFromCurrency,
+  setToCurrency
+} = currencySlice.actions
+
+export default currencySlice.reducer
+export { type CurrencyState }
