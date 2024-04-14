7f2ef2c041bd553f58abcedab82ab7f46e717f8a
Кирилл Попов <diplomatgmg@gmail.com>
2024-04-14 20:38:48 +0300

    Реализован ввод денег

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,11 +1,22 @@
-import React, { type ReactElement } from 'react'
+import React, { type ReactElement, useState } from 'react'
 
 const FormInput = (): ReactElement => {
+  const [input, setInput] = useState<string>('')
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInput(e.target.value)
+  }
+
   return (
-   <label className="converter__label">
-     <span className="converter__label-text">Enter Amount</span>
-     <input type="number" className="converter__input"/>
-   </label>
+    <label className="converter__label">
+      <span className="converter__label-text">Enter Amount</span>
+      <input
+        value={input}
+        onChange={handleChangeInput}
+        type="number"
+        className="converter__input"
+      />
+    </label>
   )
 }
 
